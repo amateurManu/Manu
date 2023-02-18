6c4e18f4e75464cd0a7353858647f4a39e1f29ef
Emmanuel Odero <oderoemmanuel6@gmail.com>
2023-02-18 21:24:13 +0300

    push

--- diagonalDifference/diagonalDifference.js
+++ diagonalDifference/diagonalDifference.js
@@ -1,4 +1,10 @@
 function diagonalDifference(arr) {
     // Write your code here
-
+    let primary = 0;
+    let secondary = 0;
+    for (let i=0; i<arr.length; i++){
+        primary += arr[i][i];
+        secondary += arr[arr.length - 1 - i][i];
+    }
+    return Math.abs(primary - secondary);
 }
